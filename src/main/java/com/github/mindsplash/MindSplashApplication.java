e9082f51f8243b0f902221abbb5a1366812f4f64
GypsyJR777 <ivan.demidov.01@list.ru>
2023-05-12 22:34:17 +0300

    init

---
+++ src/main/java/com/github/mindsplash/MindSplashApplication.java
@@ -0,0 +1,13 @@
+package com.github.mindsplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindSplashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindSplashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mindsplash/MindSplashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mindsplash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindSplashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
